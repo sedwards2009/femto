59296e5bfebb86a9e404635a6191771718beaaad
Simon Edwards <simon@simonzone.com>
2025-09-24 20:37:45 +0200

    Fall back to internal clipboard on *nix if WayLand and X11 are missing

--- clipboard.go
+++ clipboard.go
@@ -1,22 +1,46 @@
 package femto
 
 import (
+	"os"
+	"runtime"
+
 	"github.com/atotto/clipboard"
 )
 
 var internalClipboard string
+var useInternalClipboard bool
 
 func clipboardReadAll() (string, error) {
-	if clipboard.Unsupported {
+	if useInternalClipboard {
 		return internalClipboard, nil
 	}
 	return clipboard.ReadAll()
 }
 
 func clipboardWriteAll(s string) error {
-	if clipboard.Unsupported {
+	if useInternalClipboard {
 		internalClipboard = s
 		return nil
 	}
 	return clipboard.WriteAll(s)
+}
+
+func isUnix() bool {
+	return runtime.GOOS == "linux" || runtime.GOOS == "openbsd" || runtime.GOOS == "netbsd"
+}
+
+func isX11Present() bool {
+	return os.Getenv("DISPLAY") != ""
+}
+
+func isWaylandPresent() bool {
+	return os.Getenv("WAYLAND_DISPLAY") != ""
+}
+
+func init() {
+	useInternalClipboard = clipboard.Unsupported
+
+	if !clipboard.Unsupported && isUnix() && !(isWaylandPresent() || isX11Present()) {
+		useInternalClipboard = true
+	}
 }
